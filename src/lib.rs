6f071873ac359b85b44411193678673c4d6dc7dc
amtoine <stevan.antoine@gmail.com>
2023-08-15 11:13:39 +0200

    add a base crate

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
+use nu_protocol::{Category, PluginExample, PluginSignature, Type, Value};
+
+pub struct Explore;
+
+impl Plugin for Explore {
+    fn signature(&self) -> Vec<PluginSignature> {
+        vec![PluginSignature::build("explore")
+            .usage("TODO")
+            .input_output_type(Type::Any, Type::Nothing)
+            .plugin_examples(vec![PluginExample {
+                example: "open Cargo.toml | explore".into(),
+                description: "TODO".into(),
+                result: None,
+            }])
+            .category(Category::Experimental)]
+    }
+
+    fn run(
+        &mut self,
+        name: &str,
+        call: &EvaluatedCall,
+        input: &Value,
+    ) -> Result<Value, LabeledError> {
+        match name {
+            "explore" => explore(call, input),
+            _ => Err(LabeledError {
+                label: "Plugin call with wrong name signature".into(),
+                msg: "the signature used to call the plugin does not match any name in the plugin signature vector".into(),
+                span: Some(call.head),
+            }),
+        }
+    }
+}
+
+fn explore(call: &EvaluatedCall, _input: &Value) -> Result<Value, LabeledError> {
+    Ok(Value::nothing(call.head))
+}
