fd9b9674d4932d0a8f57b4ffba8f95d5f2f362bf
amtoine <stevan.antoine@gmail.com>
2023-08-16 18:49:07 +0200

    add a config to change the colors of the bar

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use crossterm::{
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
 };
-use ratatui::{prelude::CrosstermBackend, Terminal};
+use ratatui::{prelude::CrosstermBackend, style::Color, Terminal};
 
 use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
 use nu_protocol::{Category, PluginExample, PluginSignature, Type, Value};
@@ -41,8 +41,15 @@ impl Plugin for Explore {
 }
 
 fn explore(call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
+    let config = Config {
+        status_bar: StatusBarConfig {
+            background: Color::White,
+            foreground: Color::Black,
+        },
+    };
+
     let mut terminal = setup_terminal().context("setup failed").unwrap();
-    run(&mut terminal, input)
+    run(&mut terminal, input, &config)
         .context("app loop failed")
         .unwrap();
     restore_terminal(&mut terminal)
@@ -75,11 +82,24 @@ impl State {
     }
 }
 
-fn run(terminal: &mut Terminal<CrosstermBackend<console::Term>>, input: &Value) -> Result<()> {
+struct StatusBarConfig {
+    background: Color,
+    foreground: Color,
+}
+
+struct Config {
+    status_bar: StatusBarConfig,
+}
+
+fn run(
+    terminal: &mut Terminal<CrosstermBackend<console::Term>>,
+    input: &Value,
+    config: &Config,
+) -> Result<()> {
     let mut state = State::default();
 
     loop {
-        terminal.draw(|frame| render::ui(frame, input, &state))?;
+        terminal.draw(|frame| render::ui(frame, input, &state, config))?;
         match console::Term::stderr().read_char()? {
             'q' => break,
             'i' => state = State::Insert,
@@ -93,22 +113,23 @@ fn run(terminal: &mut Terminal<CrosstermBackend<console::Term>>, input: &Value) 
 mod render {
     use ratatui::{
         prelude::{Alignment, CrosstermBackend, Rect},
-        style::{Color, Style},
+        style::Style,
         widgets::Paragraph,
         Frame,
     };
 
     use nu_protocol::Value;
 
-    use super::State;
+    use super::{Config, State};
 
     pub(super) fn ui(
         frame: &mut Frame<CrosstermBackend<console::Term>>,
         input: &Value,
         state: &State,
+        config: &Config,
     ) {
         data(frame, input);
-        status_bar(frame, state);
+        status_bar(frame, state, config);
     }
 
     fn data(frame: &mut Frame<CrosstermBackend<console::Term>>, data: &Value) {
@@ -120,7 +141,11 @@ mod render {
         );
     }
 
-    fn status_bar(frame: &mut Frame<CrosstermBackend<console::Term>>, status: &State) {
+    fn status_bar(
+        frame: &mut Frame<CrosstermBackend<console::Term>>,
+        status: &State,
+        config: &Config,
+    ) {
         let rect_bottom_bar = Rect::new(0, frame.size().height - 1, frame.size().width, 1);
 
         let status = match status {
@@ -130,14 +155,22 @@ mod render {
 
         frame.render_widget(
             Paragraph::new(status)
-                .style(Style::default().fg(Color::Black).bg(Color::White))
+                .style(
+                    Style::default()
+                        .fg(config.status_bar.foreground)
+                        .bg(config.status_bar.background),
+                )
                 .alignment(Alignment::Left),
             rect_bottom_bar,
         );
 
         frame.render_widget(
             Paragraph::new("q to quit")
-                .style(Style::default().fg(Color::Black).bg(Color::White))
+                .style(
+                    Style::default()
+                        .fg(config.status_bar.foreground)
+                        .bg(config.status_bar.background),
+                )
                 .alignment(Alignment::Right),
             rect_bottom_bar,
         );
