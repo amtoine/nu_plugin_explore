783e488e18b3a042c3cadefd9d423663c149a838
amtoine <stevan.antoine@gmail.com>
2023-08-17 18:41:41 +0200

    refactor to going up and down in a function with direction

--- src/lib.rs
+++ src/lib.rs
@@ -151,6 +151,11 @@ struct Config {
     keybindings: KeyBindingsMap,
 }
 
+enum Direction {
+    Down,
+    Up,
+}
+
 fn run(
     terminal: &mut Terminal<CrosstermBackend<console::Term>>,
     input: &Value,
@@ -185,107 +190,9 @@ fn run(
         } else if char == config.keybindings.normal {
             state.mode = Mode::Normal;
         } else if char == 'j' {
-            let current = state.cell_path.members.pop();
-
-            match input
-                .clone()
-                .follow_cell_path(&state.cell_path.members, false)
-            {
-                Ok(Value::List { vals, .. }) => {
-                    let new = match current {
-                        Some(PathMember::Int {
-                            val,
-                            span,
-                            optional,
-                        }) => PathMember::Int {
-                            val: if vals.is_empty() {
-                                val
-                            } else {
-                                (val + 1 + vals.len()) % vals.len()
-                            },
-                            span,
-                            optional,
-                        },
-                        None => panic!("unexpected error when unpacking current cell path"),
-                        _ => panic!("current should be an integer path member"),
-                    };
-                    state.cell_path.members.push(new);
-                }
-                Ok(Value::Record { cols, .. }) => {
-                    let new = match current {
-                        Some(PathMember::String {
-                            val,
-                            span,
-                            optional,
-                        }) => PathMember::String {
-                            val: if cols.is_empty() {
-                                "".into()
-                            } else {
-                                let index = cols.iter().position(|x| x == &val).unwrap();
-                                cols[(index + 1 + cols.len()) % cols.len()].clone()
-                            },
-                            span,
-                            optional,
-                        },
-                        None => panic!("unexpected error when unpacking current cell path"),
-                        _ => panic!("current should be an string path member"),
-                    };
-                    state.cell_path.members.push(new);
-                }
-                Err(_) => panic!("unexpected error when following cell path"),
-                _ => {}
-            }
+            go_up_or_down_in_data(&mut state, input, Direction::Down);
         } else if char == 'k' {
-            let current = state.cell_path.members.pop();
-
-            match input
-                .clone()
-                .follow_cell_path(&state.cell_path.members, false)
-            {
-                Ok(Value::List { vals, .. }) => {
-                    let new = match current {
-                        Some(PathMember::Int {
-                            val,
-                            span,
-                            optional,
-                        }) => PathMember::Int {
-                            val: if vals.is_empty() {
-                                val
-                            } else {
-                                (val - 1 + vals.len()) % vals.len()
-                            },
-                            span,
-                            optional,
-                        },
-                        None => panic!("unexpected error when unpacking current cell path"),
-                        _ => panic!("current should be an integer path member"),
-                    };
-                    state.cell_path.members.push(new);
-                }
-                Ok(Value::Record { cols, .. }) => {
-                    let new = match current {
-                        Some(PathMember::String {
-                            val,
-                            span,
-                            optional,
-                        }) => PathMember::String {
-                            val: if cols.is_empty() {
-                                "".into()
-                            } else {
-                                let index = cols.iter().position(|x| x == &val).unwrap();
-                                cols[(index - 1 + cols.len()) % cols.len()].clone()
-                            },
-                            span,
-                            optional,
-                        },
-                        None => panic!("unexpected error when unpacking current cell path"),
-                        _ => panic!("current should be an string path member"),
-                    };
-                    state.cell_path.members.push(new);
-                }
-                Err(_) => panic!("unexpected error when following cell path"),
-                _ => {}
-            }
+            go_up_or_down_in_data(&mut state, input, Direction::Up);
         } else if char == 'l' {
             match input
                 .clone()
@@ -316,6 +223,64 @@ fn run(
         }
     }
     Ok(())
+}
+
+fn go_up_or_down_in_data(state: &mut State, input: &Value, direction: Direction) {
+    let direction = match direction {
+        Direction::Up => usize::MAX,
+        Direction::Down => 1,
+    };
+
+    let current = state.cell_path.members.pop();
+
+    match input
+        .clone()
+        .follow_cell_path(&state.cell_path.members, false)
+    {
+        Ok(Value::List { vals, .. }) => {
+            let new = match current {
+                Some(PathMember::Int {
+                    val,
+                    span,
+                    optional,
+                }) => PathMember::Int {
+                    val: if vals.is_empty() {
+                        val
+                    } else {
+                        (val + direction + vals.len()) % vals.len()
+                    },
+                    span,
+                    optional,
+                },
+                None => panic!("unexpected error when unpacking current cell path"),
+                _ => panic!("current should be an integer path member"),
+            };
+            state.cell_path.members.push(new);
+        }
+        Ok(Value::Record { cols, .. }) => {
+            let new = match current {
+                Some(PathMember::String {
+                    val,
+                    span,
+                    optional,
+                }) => PathMember::String {
+                    val: if cols.is_empty() {
+                        "".into()
+                    } else {
+                        let index = cols.iter().position(|x| x == &val).unwrap();
+                        cols[(index + direction + cols.len()) % cols.len()].clone()
+                    },
+                    span,
+                    optional,
+                },
+                None => panic!("unexpected error when unpacking current cell path"),
+                _ => panic!("current should be an string path member"),
+            };
+            state.cell_path.members.push(new);
+        }
+        Err(_) => panic!("unexpected error when following cell path"),
+        _ => {}
+    }
 }
 
 mod tui {
