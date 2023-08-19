40ed72ea9de43cabea9667e9f2d91ac91509d795
amtoine <stevan.antoine@gmail.com>
2023-08-19 13:04:21 +0200

    add documentation to navigation.rs

--- src/navigation.rs
+++ src/navigation.rs
@@ -1,12 +1,28 @@
+//! navigate in the data in all directions
 use nu_protocol::{ast::PathMember, Span, Value};
 
 use super::app::State;
 
+/// specify a vertical direction in which to go in the data
 pub(super) enum Direction {
+    /// go one row down in the data
     Down,
+    /// go one row up in the data
     Up,
 }
 
+/// go up or down in the data
+///
+/// depending on the direction (see [`Direction`]), this function will
+/// - early return if the user is already at the bottom => this is to avoid the confusing following
+/// situation: you are at the bottom of the data, looking at one item in a list, without this early
+/// return, you'd be able to scroll the list without seeing it as a whole... confusing, right?
+/// - cycle the list indices or the record column names => the index / column will wrap around
+///
+/// > :bulb: **Note**  
+/// > this function will only modify the last element of the state's *cell path* either by
+/// > - not doing anything
+/// > - poping the last element to know where we are and then pushing back the new element
 pub(super) fn go_up_or_down_in_data(state: &mut State, input: &Value, direction: Direction) {
     if state.bottom {
         return ();
@@ -69,6 +85,12 @@ pub(super) fn go_up_or_down_in_data(state: &mut State, input: &Value, direction:
     }
 }
 
+/// go one level deeper in the data
+///
+/// > :bulb: **Note**  
+/// > this function will
+/// > - push a new *cell path* member to the state if there is more depth ahead
+/// > - mark the state as *at the bottom* if the value at the new depth is of a simple type
 pub(super) fn go_deeper_in_data(state: &mut State, input: &Value) {
     match input
         .clone()
@@ -89,6 +111,11 @@ pub(super) fn go_deeper_in_data(state: &mut State, input: &Value) {
     }
 }
 
+/// pop one level of depth from the data
+///
+/// > :bulb: **Note**
+/// > - the state is always marked as *not at the bottom*
+/// > - the state *cell path* can have it's last member popped if possible
 pub(super) fn go_back_in_data(state: &mut State) {
     if !state.bottom & (state.cell_path.members.len() > 1) {
         state.cell_path.members.pop();
