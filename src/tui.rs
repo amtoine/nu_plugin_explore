17b43f9c36670f6db8ea7e8f1b3892aa2e344f08
amtoine <stevan.antoine@gmail.com>
2023-08-21 18:59:51 +0200

    add `is_table` to tell if a `Value` is a table
    this commit adds a test for a few cases.

--- src/tui.rs
+++ src/tui.rs
@@ -191,6 +191,27 @@ fn repr_data(data: &Value, cell_path: &[PathMember]) -> Vec<DataRowRepr> {
     }
 }
 
+/// TODO: documentation
+fn is_table(value: &Value, cell_path: &[PathMember]) -> Option<bool> {
+    match value.clone().follow_cell_path(&cell_path, false) {
+        Ok(Value::List { vals, .. }) => {
+            if vals.is_empty() {
+                Some(false)
+            } else {
+                match vals[0] {
+                    Value::Record { .. } => {
+                        let first = vals[0].get_type().to_string();
+                        Some(vals.iter().all(|v| v.get_type().to_string() == first))
+                    }
+                    _ => Some(false),
+                }
+            }
+        }
+        Ok(_) => Some(false),
+        Err(_) => None,
+    }
+}
+
 /// render the whole data
 ///
 /// the layout can be changed from [`crate::config::Config::layout`].
@@ -492,7 +513,7 @@ fn render_status_bar(
 mod tests {
     use nu_protocol::Value;
 
-    use super::{repr_data, repr_list, repr_record, repr_simple_value, DataRowRepr};
+    use super::{is_table, repr_data, repr_list, repr_record, repr_simple_value, DataRowRepr};
 
     #[test]
     fn simple_value() {
@@ -591,5 +612,24 @@ mod tests {
             DataRowRepr::named("i", "123", "int"),
         ];
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn is_a_table() {
+        #[rustfmt::skip]
+        let table = Value::test_list(vec![
+            Value::test_record(vec!["a", "b"], vec![Value::test_string("a"), Value::test_int(1)]),
+            Value::test_record(vec!["a", "b"], vec![Value::test_string("a"), Value::test_int(1)]),
+        ]);
+        assert_eq!(is_table(&table, &[]), Some(true));
+
+        #[rustfmt::skip]
+        let not_a_table = Value::test_list(vec![
+            Value::test_record(vec!["a"], vec![Value::test_string("a")]),
+            Value::test_record(vec!["a", "b"], vec![Value::test_string("a"), Value::test_int(1)]),
+        ]);
+        assert_eq!(is_table(&not_a_table, &[]), Some(false));
+
+        assert_eq!(is_table(&Value::test_int(0), &[]), Some(false));
     }
 }
