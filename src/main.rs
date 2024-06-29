9be16d9ead3699932376d8e2a54b96145b9af19d
Antoine Stevan <44101798+amtoine@users.noreply.github.com>
2024-06-29 11:37:17 +0200

    bump to 0.95.0 (#70)
    - implement `Plugin.version`
    - bump dependencies
    - bump version
    - update lock file

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ impl Plugin for ExplorePlugin {
     fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![Box::new(Explore)]
     }
+
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
 }
 
 struct Explore;
